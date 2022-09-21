24dbeb179ba7f690b6392d92389104ed60141f0f
AsadKhatri1 <hafizasad398@gmail.com>
2022-09-21 14:35:10 +0500

    passenger app
    my passenger app

---
+++ index.js
@@ -0,0 +1,29 @@
+let age=18;
+// console.log(age)
+
+let dogYear= 7
+let myDogAge=age*dogYear
+console.log(myDogAge);
+
+let bonusPoints=50;
+
+bonusPoints=bonusPoints+50
+console.log(bonusPoints)
+
+bonusPoints=bonusPoints-75
+console.log(bonusPoints)
+
+bonusPoints=bonusPoints+45
+console.log(bonusPoints)
+
+
+let count=0
+function func1(){
+    
+    count=count+1
+    para.innerHTML='The number of passengers are '+[count]
+}
+function func2(){
+    count=count-1
+    para.innerHTML='The number of passengers are '+[count]
+}
